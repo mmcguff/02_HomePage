7ce84689ce6fc68bc075f6487330828dde4b9ae7
GOVERNET\mmcguff <mmcguff@governet.net>
2017-09-20 08:28:53 -0600

    Making some javscript adjustments.

--- javascript.js
+++ javascript.js
@@ -20,6 +20,6 @@ function openCity(cityName, elmnt, color) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
 
 
